5bb16dd6a84f1e0557f937ff56d395848be8370b
Ben Eisner <ben.a.eisner@gmail.com>
2023-09-23 16:52:26 -0400

    added --no-check-certificate

--- scripts/download_bottle_data.sh
+++ scripts/download_bottle_data.sh
@@ -1,7 +1,7 @@
 if [ -z $NDF_SOURCE_DIR ]; then echo 'Please source "ndf_env.sh" first'
 else
-wget -O ndf_bottle_data.tar.gz https://www.dropbox.com/s/by38ryktxkcqxcx/ndf_test_bottle_data.tar.gz?dl=0
-wget -O ndf_occ_data.tar.gz https://www.dropbox.com/s/ok4fb045z7v8cpp/ndf_occ_data.tar.gz?dl=0
+wget -O ndf_bottle_data.tar.gz https://www.dropbox.com/s/by38ryktxkcqxcx/ndf_test_bottle_data.tar.gz?dl=0 --no-check-certificate
+wget -O ndf_occ_data.tar.gz https://www.dropbox.com/s/ok4fb045z7v8cpp/ndf_occ_data.tar.gz?dl=0 --no-check-certificate
 mkdir -p $NDF_SOURCE_DIR/data/training_data
 mv ndf_*_data.tar.gz $NDF_SOURCE_DIR/data/training_data
 cd $NDF_SOURCE_DIR/data/training_data
